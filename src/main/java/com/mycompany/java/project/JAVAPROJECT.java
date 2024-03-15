d29b6423fd91817078e4047f1157e89254a5c569
BASSAM <BASSAM@DESKTOP-6PCF9N8>
2024-03-15 12:57:45 +0000

    JAVA-PROJECT

---
+++ src/main/java/com/mycompany/java/project/JAVAPROJECT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.project;
+
+/**
+ *
+ * @author BASSAM
+ */
+public class JAVAPROJECT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
