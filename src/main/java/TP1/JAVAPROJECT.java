7dc7a7df2627f26a53631cd6af466f6c1407bca4
BASSAM <BASSAM@DESKTOP-6PCF9N8>
2024-03-15 13:57:25 +0000

    JAVA-PROJECT

--- src/main/java/TP1/JAVAPROJECT.java
+++ src/main/java/TP1/JAVAPROJECT.java
@@ -3,6 +3,7 @@
  */
 
 package TP1;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,16 @@ package TP1;
 public class JAVAPROJECT {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        float f = (float) a / b;
+        System.out.println("la somme est :" + c);
+        System.out.println("la soustraction est :" + d);
+        System.out.println("la produit est :" + e);
+        System.out.println("la division est :" + f);
     }
 }
